deed485f5f9ecd04c773e7b23e02cb52402e453b
skeral <96560313+skeral@users.noreply.github.com>
2024-01-26 09:58:47 +0900

    Update trainer.py

--- common/trainer.py
+++ common/trainer.py
@@ -5,7 +5,8 @@ import numpy as np
 from common.optimizer import *
 
 class Trainer:
-    """신경망 훈련을 대신 해주는 클래스
+    """
+    Class which encapsulates the process of training a neural network
     """
     def __init__(self, network, x_train, t_train, x_test, t_test,
                  epochs=20, mini_batch_size=100,
@@ -21,14 +22,19 @@ class Trainer:
         self.batch_size = mini_batch_size
         self.evaluate_sample_num_per_epoch = evaluate_sample_num_per_epoch
 
-        # optimzer
+        # Optimizer Dictionary
+        # maps string keys to optimizer classes and then creates an instance of the optimizer using the provided parameters.
         optimizer_class_dict = {'sgd':SGD, 'momentum':Momentum, 'nesterov':Nesterov,
                                 'adagrad':AdaGrad, 'rmsprpo':RMSprop, 'adam':Adam}
+        
         self.optimizer = optimizer_class_dict[optimizer.lower()](**optimizer_param)
         
         self.train_size = x_train.shape[0]
         self.iter_per_epoch = max(self.train_size / mini_batch_size, 1)
+
+        # Calculates the max number of iterations for training
         self.max_iter = int(epochs * self.iter_per_epoch)
+
         self.current_iter = 0
         self.current_epoch = 0
         
@@ -37,22 +43,37 @@ class Trainer:
         self.test_acc_list = []
 
     def train_step(self):
+        """
+        Executes a single step of training
+        """
+
+        # Randomly selects a batch of indices from the training set
         batch_mask = np.random.choice(self.train_size, self.batch_size)
+
+        # Select a subset of the training data
         x_batch = self.x_train[batch_mask]
         t_batch = self.t_train[batch_mask]
         
+        # Calculates the gradient of the network's loss with respect to its parameters on its batch
         grads = self.network.gradient(x_batch, t_batch)
+
+        # Update the network's parameters based on the gradients
         self.optimizer.update(self.network.params, grads)
         
+        # Calculate and records the training loss
         loss = self.network.loss(x_batch, t_batch)
         self.train_loss_list.append(loss)
+
         if self.verbose: print("train loss:" + str(loss))
         
+        # Checks if an epoch has completed
+        # If an epoch is completed, evaluates the network's performance on both the training and test sets.
         if self.current_iter % self.iter_per_epoch == 0:
             self.current_epoch += 1
             
             x_train_sample, t_train_sample = self.x_train, self.t_train
             x_test_sample, t_test_sample = self.x_test, self.t_test
+
             if not self.evaluate_sample_num_per_epoch is None:
                 t = self.evaluate_sample_num_per_epoch
                 x_train_sample, t_train_sample = self.x_train[:t], self.t_train[:t]
@@ -67,9 +88,15 @@ class Trainer:
         self.current_iter += 1
 
     def train(self):
+        """
+        Performs the entire training process
+        """
+
+        # Runs the training for the calculated number of iterations
         for i in range(self.max_iter):
             self.train_step()
-
+        
+        # Evaluate the final accuracy on the test set
         test_acc = self.network.accuracy(self.x_test, self.t_test)
 
         if self.verbose:
